8cae86b6e29801e3744ab3a2f66f555793eed696
alexgmzdev <contacto.alejandrogimenez@gmail.com>
2024-07-16 13:00:11 +0200

    Added file ClientesService, modified Client Controller, and Repo

--- 01 - Backend/src/main/java/com/example/APIRestAerolinea/repositorios/ClienteRepository.java
+++ 01 - Backend/src/main/java/com/example/APIRestAerolinea/repositorios/ClienteRepository.java
@@ -3,7 +3,10 @@ package com.example.APIRestAerolinea.repositorios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.APIRestAerolinea.modelos.Cliente;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
 }
--- 01 - Backend/src/main/java/com/example/APIRestAerolinea/servicios/ClientesService.java
+++ 01 - Backend/src/main/java/com/example/APIRestAerolinea/servicios/ClientesService.java
@@ -1,4 +1,33 @@
 package com.example.APIRestAerolinea.servicios;
 
+import com.example.APIRestAerolinea.modelos.Cliente;
+import com.example.APIRestAerolinea.repositorios.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
 public class ClientesService {
+
+    @Autowired
+    ClienteRepository clientesRepository;
+
+    public ArrayList<Cliente> getClientes(){
+        return (ArrayList<Cliente>) clientesRepository.findAll();
+    }
+
+    public Cliente guardarCliente(Cliente cliente){
+        return clientesRepository.save(cliente);
+    }
+
+    public Optional<Cliente> getById(Long id){
+        return clientesRepository.findById(id);
+    }
+
+    public Cliente updateById (Cliente request, Long id){
+        Cliente cliente = clientesRepository.findById(id).get();
+
+        cliente.setEmail(request.getEmail());
+        return cliente;
+    }
 }
